090e5d9e5f79513f2e44886d0c3793ea0fa0cb74
Michael Chen <git@cnml.de>
2022-06-24 15:35:55 +0200

    Add project files.

---
+++ GDCursorLock/Native.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using PInvoke;
+
+namespace GDCursorLock;
+
+public static class Native {
+    private static void ApiException(string method) => throw new Exception($"API call failed for method '{method}'!");
+
+    [DllImport("user32.dll", EntryPoint = "GetCursorPos")]
+    private static extern bool _GetCursorPos(out POINT point);
+    public static void GetCursorPos(out POINT point) {
+        if (!_GetCursorPos(out point)) ApiException(Helpers.GetCallerName());
+    }
+
+    [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+    private static extern bool _SetCursorPos(int X, int Y);
+    public static void SetCursorPos(in POINT point) => SetCursorPos(point.x, point.y);
+    public static void SetCursorPos(int x, int y) {
+        if (!_SetCursorPos(x, y)) ApiException(Helpers.GetCallerName());
+    }
+
+    [DllImport("user32.dll", EntryPoint = "GetWindowRect")]
+    private static extern bool _GetWindowRect(IntPtr hWnd, out RECT lpRect);
+    public static void GetWindowRect(IntPtr hwnd, out RECT lpRect) {
+        if (!_GetWindowRect(hwnd, out lpRect)) ApiException(Helpers.GetCallerName());
+    }
+
+    [DllImport("user32.dll", EntryPoint = "ClipCursor")]
+    private static extern bool _ClipCursor(in RECT lpRect);
+    public static void ClipCursor(in RECT lpRect) {
+        if (!_ClipCursor(in lpRect)) ApiException(Helpers.GetCallerName());
+    }
+
+    [DllImport("user32.dll", EntryPoint = "GetClipCursor")]
+    private static extern bool _GetClipCursor(out RECT lpRect);
+    public static void GetClipCursor(out RECT lpRect) {
+        if (!_GetClipCursor(out lpRect)) ApiException(Helpers.GetCallerName());
+    }
+
+    [DllImport("user32.dll", EntryPoint = "GetForegroundWindow")]
+    [SuppressMessage("Interoperability", "CA1401:P/Invokes should not be visible", Justification = "Method is safe to use.")]
+    public static extern IntPtr GetForegroundWindow();
+
+}
+
+public static class Helpers {
+    public static string GetCallerName([CallerMemberName] string caller = default!)
+        => caller ?? throw new InvalidProgramException("CallerMemberName was null!");
+}
---
+++ GDCursorLock/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using GDCursorLock;
+using PInvoke;
+
+RECT rect;
+POINT point;
+
+Console.WriteLine("GD Cursor Lock by cn-ml");
+Console.WriteLine("Your Cursor will now be fixed to Geometry Dash if the application is focused.");
+Console.WriteLine("You can always Alt+Tab out of Geometry Dash");
+Console.WriteLine("Press Ctrl+C to quit");
+
+CancellationTokenSource cts = new CancellationTokenSource();
+void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e) {
+    cts.Cancel();
+    e.Cancel = true;
+}
+Console.CancelKeyPress += Console_CancelKeyPress;
+
+while (true) {
+    // without delay this loops ~100/s
+    await Task.Delay(60, cts.Token);
+    if (Process.GetProcessesByName("GeometryDash").SingleOrDefault() is not Process process) {
+        Log("Process not running!");
+        continue;
+    }
+    if (process.MainWindowHandle == IntPtr.Zero) {
+        Log("Process has no main window!");
+        continue;
+    }
+    if (process.MainWindowHandle != Native.GetForegroundWindow()) {
+        Log("Process window is not focused!");
+        continue;
+    }
+    Native.GetCursorPos(out point);
+    Native.GetWindowRect(process.MainWindowHandle, out rect);
+    if (IsInside(in point, in rect)) {
+        Log("Cursor is in window");
+        continue;
+    }
+    Log("Cursor is outside window and will be reset!");
+    SetToCenter(in rect, out point);
+    Native.SetCursorPos(in point);
+}
+
+Console.WriteLine("Goodbye");
+
+[Conditional("DEBUG")]
+static void Log(string message) => Console.WriteLine(message);
+
+static void SetToCenter(in RECT rect, out POINT point) {
+    point.x = (rect.right - rect.left) / 2;
+    point.y = (rect.bottom - rect.top) / 2;
+}
+
+static bool IsInside(in POINT point, in RECT rect)
+    => point.x >= rect.left && point.x < rect.right
+    && point.y >= rect.top && point.y < rect.bottom;
